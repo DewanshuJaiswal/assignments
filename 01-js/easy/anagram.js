625eefa216cf1f796e493f3a9438b944686b2245
Dewanshu Jaiswal <98215156+DewanshuJaiswal@users.noreply.github.com>
2023-12-07 03:54:46 +0530

    Update anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,6 +5,27 @@
 */
 
 function isAnagram(str1, str2) {
+  // removing all spaces and converting to lowercase
+  const newStr1 = str1.toLowerCase();
+  const newStr2 = str2.toLowerCase();
+
+  if(newStr1.length !== newStr2.length)return false;
+
+  let freq = new Array(26).fill(0);
+
+  for(let i = 0; i < newStr1.length; i++)
+  {
+      freq[newStr1[i].charCodeAt(0) - 'a'.charCodeAt(0)]++;   // charCodeAt(0) used for getting ascii values
+  }
+  for(let i = 0; i < newStr2.length; i++)
+  {
+      freq[newStr2[i].charCodeAt(0) - 'a'.charCodeAt(0)]--;
+  }
+  for(let i = 0; i < 26; i++)
+  {
+      if(freq[i] !== 0)return false;
+  }
+  return true;
 
 }
 
